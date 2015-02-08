69f42950dc608e77e2b59a0bda84219dee788541
ZwEin <zwein27@gmail.com>
2015-02-07 21:21:44 -0800

    Initial Commit

---
+++ mac15wr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mac15wr
+//
+//  Created by 李言可 on 15/2/7.
+//  Copyright (c) 2015年 mac15wr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mac15wr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mac15wr
+//
+//  Created by 李言可 on 15/2/7.
+//  Copyright (c) 2015年 mac15wr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
